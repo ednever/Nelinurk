7ab37ca6629ea107558cabb357923cd4d0292dcd
opilane <opilane@HP-CZC6297BBL>
2022-03-02 10:55:37 +0200

    Add project files.

---
+++ Nelinurk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+    public class Nelinurkk
+    {
+        double korgus, laius;
+        Tyyp tyyp;
+
+        public Nelinurkk() { }
+        public Nelinurkk(double Korgus, double Laius, Tyyp tyyp)
+        {
+            korgus = Korgus;
+            laius = Laius;
+            this.tyyp = tyyp;
+        }
+        public double Korgus
+        {
+            set { korgus = value; }
+            get { return korgus; }
+        }
+        public double Laius
+        {
+            set { laius = value; }
+            get { return laius; }
+        }
+        public Tyyp Tyyp
+        {
+            get { return tyyp; }
+        }
+        public double Pindala()
+        {
+            double pindala = korgus * laius;
+            return pindala;
+        }
+        public double Perimetr()
+        {
+            double P = korgus * 2 + laius * 2;
+            return P;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nelinurkk Kujund = new Nelinurkk();
+            Console.WriteLine("Ruut/1, ristkülik/2, romb/3, rööpkülik/4");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Sisetage nelinurga kõrgus ja laius");
+            Kujund.Korgus = double.Parse(Console.ReadLine());
+            Kujund.Laius = double.Parse(Console.ReadLine());
+            double P = Kujund.Perimetr();
+            double pindala = Kujund.Pindala();
+            if (symbol == "1")
+            {
+                Console.WriteLine("Nice");
+            }
+            else if (symbol == "2")
+            {
+                
+                
+            }
+            else if (symbol == "3")
+            {
+
+            }
+            else if (symbol == "4")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Vale andmetüüp");
+            }
+            Console.WriteLine($"Nelinurga ümbermõõt - {P} cm ja pindala - {pindala} cm2");
+
+
+            Console.ReadLine();
+        }
+    }
+}
