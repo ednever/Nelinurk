14a430bccc52b19fd9be184de829eb638c400192
opilane <opilane@HP-CZC6297BBL>
2022-03-07 10:08:06 +0200

    Done

--- Nelinurk.cs
+++ Nelinurk.cs
@@ -18,6 +18,10 @@ namespace Nelinurk
             laius = Laius;
             this.tyyp = tyyp;
         }
+        public Nelinurkk(Tyyp tyyp)
+        {
+            this.tyyp = tyyp;
+        }
         public double Korgus
         {
             set { korgus = value; }
@@ -41,6 +45,39 @@ namespace Nelinurk
         {
             double P = korgus * 2 + laius * 2;
             return P;
+        }
+        public void Figura()
+        {
+            Console.WriteLine("Ruut/1, ristkülik/2, romb/3, rööpkülik/4");
+            string symbol = Console.ReadLine();
+            if (symbol == "1")
+            {
+                Nelinurkk ruut = new Nelinurkk(korgus, laius, Tyyp.ruut);
+                Console.WriteLine($"Nelinurga ümbermõõt - {ruut.Perimetr()} cm, pindala - {ruut.Pindala()} cm2 ja nelinurga tüüp on {Tyyp.ruut}");
+            }
+            else if (symbol == "2")
+            {
+                Nelinurkk ristkylik = new Nelinurkk(korgus, laius, Tyyp.ristkylik);
+                Console.WriteLine($"Nelinurga ümbermõõt - {ristkylik.Perimetr()} cm, pindala - {ristkylik.Pindala()} cm2 ja nelinurga tüüp on {Tyyp.ristkylik}");
+            }
+            else if (symbol == "3")
+            {
+                Console.WriteLine("Palun kirjuta rombi pikkus - pindala leidmiseks");
+                double pikkus = double.Parse(Console.ReadLine());
+                Nelinurkk romb = new Nelinurkk(korgus, laius, Tyyp.romb);
+                Console.WriteLine($"Nelinurga ümbermõõt - {romb.Perimetr()} cm, pindala - {pikkus * laius} cm2 ja nelinurga tüüp on {Tyyp.romb}");
+            }
+            else if (symbol == "4")
+            {
+                Console.WriteLine("Palun kirjuta rööpküliku pikkus - pindala leidmiseks");
+                double pikkus = double.Parse(Console.ReadLine());
+                Nelinurkk roopkylik = new Nelinurkk(korgus, laius, Tyyp.roopkylik);
+                Console.WriteLine($"Nelinurga ümbermõõt - {roopkylik.Perimetr()} cm, pindala - {pikkus * laius} cm2 ja nelinurga tüüp on {Tyyp.roopkylik}");
+            }
+            else
+            {
+                Console.WriteLine("Vale andmetüüp");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,36 +11,10 @@ namespace Nelinurk
         static void Main(string[] args)
         {
             Nelinurkk Kujund = new Nelinurkk();
-            Console.WriteLine("Ruut/1, ristkülik/2, romb/3, rööpkülik/4");
-            string symbol = Console.ReadLine();
             Console.WriteLine("Sisetage nelinurga kõrgus ja laius");
             Kujund.Korgus = double.Parse(Console.ReadLine());
             Kujund.Laius = double.Parse(Console.ReadLine());
-            double P = Kujund.Perimetr();
-            double pindala = Kujund.Pindala();
-            if (symbol == "1")
-            {
-                Console.WriteLine("Nice");
-            }
-            else if (symbol == "2")
-            {
-                
-                
-            }
-            else if (symbol == "3")
-            {
-
-            }
-            else if (symbol == "4")
-            {
-
-            }
-            else
-            {
-                Console.WriteLine("Vale andmetüüp");
-            }
-            Console.WriteLine($"Nelinurga ümbermõõt - {P} cm ja pindala - {pindala} cm2");
-
+            Kujund.Figura();            
 
             Console.ReadLine();
         }
